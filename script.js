70bceaf1c5484de3cbba862b3dc76cabd446a099
waspryo <wasp2123321@gmail.com>
2019-12-28 15:21:52 +0900

    Variables - Let and Const

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ function messaging(message) {
 console.log(message)
 console.log(othermessage)
 // 外で呼ぶのをhoistingと呼ぶ
+
+
+/******************************
+ *  Variables - Let and Const
+ */
+// var
+var nice = 'I am'
+var nice = 'I am'
+// let
+let boy = '18'
+
+let title = 'h1'
+
+if (true) {
+    let message = 'hey'
+    console.log(message)
+}
+console.log(title)
+
+// const
+const onigiri = 'okaka'
+// onigiri = 'shake'
+// 再代入できない
+console.log(onigiri)
+
+const onigiriDayo = ['okaka']
+onigiriDayo.push('ume')
+console.log(onigiriDayo)
